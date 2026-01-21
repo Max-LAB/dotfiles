63a66d3f6e8dd898dc8b4ab9db84d46fe88a6c1e
Malik <Malik.Begovic@bitbw.bwl.de>
2026-01-21 15:19:17 +0100

    new try

--- .chezmoiscripts/run_once_10_install_zsh.sh
+++ .chezmoiscripts/run_once_10_install_zsh.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-if command -v zsh >/dev/null 2>&1; then
-  exit 0
+if ! command -v zsh >/dev/null 2>&1; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update
+    sudo apt install -y zsh
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install zsh
+  fi
 fi
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sudo apt update
-  sudo apt install -y zsh
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew install zsh
-fi
+ZSH_PATH="$(command -v zsh)"
 
-chsh -s "$(command -v zsh)"
+if [[ "$SHELL" != "$ZSH_PATH" ]]; then
+  echo "==> Changing default shell to zsh"
+  chsh -s "$ZSH_PATH"
+  echo "==> Logout and login required"
+fi
