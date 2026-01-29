36fb8f35f69fc07514637640e63e553f2e709efd
Malik <Malik.Begovic@bitbw.bwl.de>
2026-01-29 15:04:51 +0100

    change install zsh script

--- .chezmoiscripts/run_once_10_install_zsh.sh
+++ .chezmoiscripts/run_once_10_install_zsh.sh
@@ -2,7 +2,7 @@
 set -e
 echo "$OSTYPE"
 if ! command -v zsh >/dev/null 2>&1; then
-  if [[ "$OSTYPE" == "linux-debian"* ]] || [[ "$OSTYPE" == "linux-raspbian"* ]] || [[ "$OSTYPE" == "linux-ubuntu"* ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
     sudo apt install -y zsh
   elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -14,6 +14,6 @@ ZSH_PATH="$(command -v zsh)"
 
 if [[ "$SHELL" != "$ZSH_PATH" ]]; then
   echo "==> Changing default shell to zsh"
-  echo "==> chsh -s "$ZSH_PATH""
+  chsh -s "$ZSH_PATH"
   echo "==> Logout and login required"
 fi
